c376818300b439c5b297fc8e62d9f4976dbb30c2
pradeep <724pradeep@gmail.com>
2023-09-15 16:10:29 +0530

    state lifting done

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
-function AddUser() {
+function AddUser(props) {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    props.onAddUser(enteredUserName, enteredAge);
+    setEnteredUserName("");
+    setEnteredAge("");
   };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="usename">UserName</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="usename">UserName</label>
+        <input
+          id="username"
+          type="text"
+          onChange={userNameChangeHandler}
+          value={enteredUserName}
+          autoComplete="off"
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
+        {/* <button type="submit">Add User</button> */}
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 }
 
